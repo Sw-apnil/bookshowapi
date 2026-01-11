0cd5b87d6c52b9866ad5d7e0f96909eaae161496
Sw-apnil <swapnil414416@gmail.com>
2026-01-11 14:58:51 +0530

    made inngest fuctions to create ,update and delete user models data

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { clerkMiddleware } from "@clerk/express";
-// import { serve } from "inngest/express";
-// import { inngest, functions } from "./inngest/index.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
 import connectDB from "./configs/db.js";
 // import showRouter from "./routes/showRoutes.js";
 // import bookingRouter from "./routes/bookingRoutes.js";
@@ -30,7 +30,7 @@ app.use(clerkMiddleware());
 
 // API Routes
 app.get("/", (req, res) => res.send("Server is live!!!"));
-//app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // app.use("/api/show", showRouter);
 // app.use("/api/booking", bookingRouter);
