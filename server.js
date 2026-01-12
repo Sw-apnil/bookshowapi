d4b9c41dc448f9fa88f51d94ba1b25d98ad74a5b
Sw-apnil <swapnil414416@gmail.com>
2026-01-12 17:33:52 +0530

    created full admin rotes and controllers

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 import connectDB from "./configs/db.js";
 import showRouter from "./routes/showRoutes.js";
-// import bookingRouter from "./routes/bookingRoutes.js";
-// import adminRouter from "./routes/adminRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
+import adminRouter from "./routes/adminRoutes.js";
 // import userRouter from "./routes/userRoutes.js";
 // import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
 
@@ -35,8 +35,8 @@ app.get("/", (req, res) => res.send("Server is live!!!"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.use("/api/show", showRouter);
-// app.use("/api/booking", bookingRouter);
-// app.use("/api/admin", adminRouter);
+app.use("/api/booking", bookingRouter);
+app.use("/api/admin", adminRouter);
 // app.use("/api/user", userRouter);
 
 app.listen(port, () =>
