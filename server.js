129e13f54e400f35edb0539475efe578acb41dff
Sw-apnil <swapnil414416@gmail.com>
2026-01-12 17:49:40 +0530

    create all user controller and routes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import connectDB from "./configs/db.js";
 import showRouter from "./routes/showRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
 import adminRouter from "./routes/adminRoutes.js";
-// import userRouter from "./routes/userRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 // import { stripeWebhooks } from "./controllers/stripeWebhooks.js";
 
 const app = express();
@@ -37,7 +37,7 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/show", showRouter);
 app.use("/api/booking", bookingRouter);
 app.use("/api/admin", adminRouter);
-// app.use("/api/user", userRouter);
+app.use("/api/user", userRouter);
 
 app.listen(port, () =>
   console.log(`Server is running at http://localhost:${port}`)
